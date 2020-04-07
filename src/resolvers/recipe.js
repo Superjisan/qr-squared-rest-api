50ce9dca610920cceca44e90201089fe34819ef8
Jisan Zaman <jisan.zaman@dynamo.works>
2020-04-06 21:54:30 -0400

    reverse search author on recipe

--- src/resolvers/recipe.js
+++ src/resolvers/recipe.js
@@ -2,10 +2,19 @@ import { combineResolvers } from 'graphql-resolvers';
 import { AuthenticationError, UserInputError } from 'apollo-server';
 
 export default {
-    Query: {
-        recipes: async(parent, args, {models}) => {
-            return await models.Recipe.findAll();
-        }
-    }
+  Query: {
+    recipes: async (parent, args, { models }) => {
+      return await models.Recipe.findAll();
+    },
+  },
 
-}
+  Recipe: {
+    author: async (recipe, args, { models }) => {
+      return await models.User.findOne({
+        where: {
+          id: recipe.authorId,
+        },
+      });
+    },
+  },
+};
