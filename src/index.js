946e02e7a9bc58f376295d4a827a072656dc4a53
Jisan Zaman <jisan.zaman@dynamo.works>
2020-04-09 21:04:16 -0400

    add cors options

--- src/index.js
+++ src/index.js
@@ -22,7 +22,18 @@ if(process.env.NODE_ENV === "production") {
 
 const app = express();
 
-app.use(cors());
+var whitelist = ['http://localhost:3000']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions));
 
 app.use(morgan('dev'));
 
